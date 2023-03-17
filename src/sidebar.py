35f850f46047e238388d80db89898362db27f99d
Michael Booth <michael@databooth.com.au>
2023-03-17 12:15:31 +1100

    sidebar tweaks

--- src/sidebar.py
+++ src/sidebar.py
@@ -3,4 +3,8 @@ import streamlit as st
 
 def create_sidebar():
     st.sidebar.markdown("## __Sidebar:__")
+
+    st.sidebar.date_input(label="Choose a date:")
+
+    st.sidebar.slider(label="Take me for a slide...")
     return None
